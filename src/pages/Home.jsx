a6d259ac05e9b967ba2ec0af67f3ef900f8900ab
Piggey <pepcio3320@gmail.com>
2024-05-28 19:26:54 +0200

    img gallery do home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import { Box } from "@mui/material"
 import { OfferGallery } from "../components/OfferGallery";
 import { Location } from "../components/Location";
 import SearchForm from "../components/SearchForm";
+import { ImageGallery } from "../components/ImageGallery";
 
 export const Home = () => {
   return (
     <Box>
       <SearchForm />
       <OfferGallery />
+      <ImageGallery />
       <Location />
     </Box>
   );
