6b563aa918f52f2e3921dabf6006ebea4ebc4fa8
Piggey <pepcio3320@gmail.com>
2024-06-08 18:25:40 +0200

    unused czesc druga

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Layout } from "./Layout";
 import { Home } from "./pages/Home";
-import { Bar } from "./pages/Bar";
-import { Tickets } from "./pages/Tickets";
-import { BirthdayReservations } from "./pages/BirthdayReservations";
 import { Login } from "./pages/Login";
-import { History } from "./pages/History";
 import { Offers } from "./pages/Offers";
 import { Offer } from "./pages/Offer";
 import { Reserve } from "./pages/Reserve";
@@ -44,42 +40,10 @@ const router = createBrowserRouter([
     )
   },
   {
-    path: "/bar",
-    element: (
-      <Layout>
-        <Bar />
-      </Layout>
-    ),
-  },
-  {
-    path: "/tickets",
-    element: (
-      <Layout>
-        <Tickets />
-      </Layout>
-    ),
-  },
-  {
-    path: "/birthdays",
-    element: (
-      <Layout>
-        <BirthdayReservations />
-      </Layout>
-    ),
-  },
-  {
     path: "/login",
     element: (
       <Layout>
         <Login />
-      </Layout>
-    ),
-  },
-  {
-    path: "/history",
-    element: (
-      <Layout>
-        <History />
       </Layout>
     ),
   },
